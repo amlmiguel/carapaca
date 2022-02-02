c16b717c3fbeb2d1d1cd2ce415a84aabfc75db75
Miguel Leite <amlmiguel@gmail.com>
2022-02-02 16:01:57 -0300

    End of section 5

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,52 @@
+using API.Erros;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly StoreContext _context;
+        public BuggyController(StoreContext context)
+        {
+            _context = context;
+
+        }
+
+        [HttpGet("notfound")]
+        public ActionResult GetFoundRequest()
+        {
+            var thing = _context.Products.Find(99);
+
+            if( thing == null) 
+            {
+                return NotFound(new ApiResponse(404));
+            }
+
+            return Ok();
+        }
+
+        [HttpGet("servererror")]
+        public ActionResult GetServerError()
+        {
+            var thing = _context.Products.Find(99);
+
+            var thingToReturn = thing.ToString();
+
+            return Ok();
+        }
+
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest(new ApiResponse(404));
+        }
+
+        [HttpGet("badrequest/{id}")]
+        public ActionResult GetFoundRequest(int id)
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ API/Controllers/ErrorsController.cs
@@ -0,0 +1,16 @@
+using API.Erros;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("errors/{code}")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class ErrorsController : BaseApiController
+    {
+        public IActionResult Error(int code)
+        {
+            return new ObjectResult(new ApiResponse(code));
+        }
+
+    }
+}
