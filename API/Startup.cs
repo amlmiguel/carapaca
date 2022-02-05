6a6a61851cf6d832d9f008aed6930bcb38df3ac4
Miguel Leite <amlmiguel@gmail.com>
2022-02-05 14:47:51 -0300

    end of section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -28,6 +28,13 @@ namespace API
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors( opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +56,8 @@ namespace API
             app.UseStaticFiles();
 
             app.UseAuthorization();
+            
+            app.UseCors("CorsPolicy");
 
             app.UseSwaggerDocumentation();
 
