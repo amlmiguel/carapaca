cedafbe6accb12157e1e2cc7290ac97d49f4ada4
Miguel Leite <amlmiguel@gmail.com>
2022-02-11 11:30:54 -0300

    End of section 10

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -6,6 +6,7 @@ import { IProductColor } from '../shared/models/productColor';
 import { IProductType } from '../shared/models/productType';
 import {map} from 'rxjs/operators'
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -46,6 +47,10 @@ export class ShopService {
       })
     )
 
+  }
+
+  getProduct(id: number){
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
   }
 
   getBrands() {
